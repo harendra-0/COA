c05b033762139af25c5f59d6c15bc4b0ad4354ae
harendra-0 <98276186+harendra-0@users.noreply.github.com>
2022-01-24 00:41:57 +0530

    Create matrix_multi_language_hookup_bucket2_b.c

---
+++ Assignment 1/matrix_multi_language_hookup_bucket2_b.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<time.h>
+
+
+int main(){
+
+
+
+    int n;
+
+    scanf("%d", &n);
+
+    long long int a[n][n], b[n][n], c[n][n];
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            // scanf("%d", &a[i][j]);
+
+            a[i][j] = rand()/100;
+
+        }
+
+    }
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            // scanf("%d", &b[i][j]);
+
+            b[i][j] = rand()/100;
+
+        }
+
+    }
+    time_t t1, t2;
+    t1 = clock();
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            c[i][j] = 0;
+
+            for(int k = 0; k < n;k++){
+
+                c[i][j] = c[i][j] + a[i][k]*b[k][j]; 
+
+            }
+
+            //printf("%lld ", c[i][j]);
+
+        }
+
+        //printf("\n");
+
+    }
+    t2 = clock();
+    double time = ((double) (t2 -t1))/ CLOCKS_PER_SEC;
+    printf("%f", time);
+
+    return 0;
+
+
+
+}
